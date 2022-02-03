9cddaf9a69a9c35d2ebff344ba450fed4bdeaf17
ItzelGpe <L18270283@tuxtla.tecnm.mx>
2022-02-03 00:29:17 -0600

    Corregir-MainActivity

--- app/src/main/java/com/example/a291_e31_minicalculadora/MainActivity.java
+++ app/src/main/java/com/example/a291_e31_minicalculadora/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
             mResultado.setText((dValor1+dValor2)+ "");
         }catch (NumberFormatException e){
-            Toast.makeText(this, "Falta algun valor", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Error falta algun valor", Toast.LENGTH_SHORT).show();
         }
 
     }
